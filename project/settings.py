c3b9b0516fe699e63da5b8747d446c293a2cfabd
Henrique <heduardocr7@gmail.com>
2023-07-14 18:06:30 -0300

    config

--- project/settings.py
+++ project/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'static_files'
 
+MERIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
