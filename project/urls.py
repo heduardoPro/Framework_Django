1c983258cd346c863ef7ec4ac1f21cb5f2480c49
Henrique <heduardocr7@gmail.com>
2023-07-16 23:06:29 -0300

    urls.py configurada

--- project/urls.py
+++ project/urls.py
@@ -26,8 +26,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:  # Dev only
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
