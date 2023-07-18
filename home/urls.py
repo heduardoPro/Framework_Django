419eb9e04be335cb55073d48b4cc826f74a5cb0e
Henrique <heduardocr7@gmail.com>
2023-07-18 10:34:54 -0300

    FIM?

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views 
+from .views import ContextView, IndexView
 
 app_name = 'home'
 
-#Home
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', IndexView.as_view(), name="home"),
+    path('list/', ContextView.as_view(), name='list'),
 ] 
--- home/views.py
+++ home/views.py
@@ -1,25 +1,53 @@
+import boto3
+from botocore.exceptions import NoCredentialsError
 from django.shortcuts import render, redirect
 from .models import Image
 from .forms import ImageForm
+from django.views.generic import ListView, FormView
+from django.urls import reverse_lazy
+from project.settings import (
+     AWS_ACCESS_KEY_ID,
+     AWS_S3_REGION,
+     AWS_SECRET_ACCESS_KEY,
+     URL_SQS,
+)
 
-def home(request):
-    uploaded = False
+class IndexView(FormView):
+    template_name = 'home/index.html'
+    form_class = ImageForm
+    success_url = reverse_lazy('home:home')
 
-    if request.method == 'POST':
-        form = ImageForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            uploaded = True
-            return redirect('home:home')
-    else:
-        form = ImageForm()
+    def form_valid(self, form):
+        form.save()
+        
+        try:
+            # Create an instance of the SQS client
+            sqs = boto3.client(
+                'sqs',
+                aws_access_key_id=AWS_ACCESS_KEY_ID,
+                aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                region_name=AWS_S3_REGION
+            )
 
-        images = Image.objects.all()
+            # Send a message to the queue
+            queue_url = URL_SQS
+            message_body = 'Envio de imagem para fila'
+            response = sqs.send_message(QueueUrl=queue_url, MessageBody=message_body)
 
-    context = {
-    'form': form,
-    'images': images,
-    'uploaded': uploaded,
-    }
+            # Check the response
+            if 'MessageId' in response:
+                print('Mensagem enviada com sucesso!')
+            else:
+                print('Erro ao enviar a mensagem.')
 
-    return render(request, 'home/index.html', context)
+        except NoCredentialsError:
+            message = "Erro de autenticação ao acessar o serviço S3."
+        except Exception as e:
+            message = f"Erro ao acessar a imagem no S3: {str(e)}"
+
+        return super().form_valid(form)
+
+class ContextView(ListView):
+    model = Image
+    template_name = 'home/images.html'
+    context_object_name = 'images'
